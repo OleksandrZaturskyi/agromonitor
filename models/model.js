df9ddadac5cc26cee6f12fef5ad70fab6530dbf5
Aleksandr <zatursky.lex@gmail.com>
2019-04-24 14:01:28 +0300

    code style changes

--- models/model.js
+++ models/model.js
@@ -4,35 +4,35 @@ const uri = "mongodb://localhost:27017/";
 const db = 'agromonitor';
 
 class Model {
-    constructor (name) {
+    constructor(name) {
         this.collectionName = name;
     }
-    
-    async _connectToDB (mongoClient, dbName, collectionName) {
-        let client =  await mongoClient.connect(uri, { useNewUrlParser: true });
+
+    async _connectToDB(mongoClient, dbName, collectionName) {
+        const client = await mongoClient.connect(uri, {useNewUrlParser: true});
         return {client: client, collection: client.db(dbName).collection(collectionName)};
     }
 
-    async _tryCatchFinally (codeToCheck, operation, client) {
+    async _tryCatchFinally(opPromise, operation, client) {
         try {
-            let result = await codeToCheck;
+            const result = await opPromise;
             switch (operation) {
                 case 'create':
-                break;
+                    break;
                 case 'delete':
                     if (result.result.n === 0) {
-                        let err = new Error ("Wrong id");
-                            err.statusCode = 400;
-                            throw err;
+                        const err = new Error("Wrong id");
+                        err.statusCode = 400;
+                        throw err;
                     }
-                break;
+                    break;
                 case 'read':
                     if (!result) {
-                        let err = new Error ("Data not found");
-                            err.statusCode = 404;
-                            throw err;
+                        const err = new Error("Data not found");
+                        err.statusCode = 404;
+                        throw err;
                     }
-                break;
+                    break;
                 case 'update':
                     if (result.result.n === 1 && result.result.nModified === 0) {
                         const err = new Error('Already up to date');
@@ -43,9 +43,9 @@ class Model {
                         err.statusCode = 400;
                         throw err;
                     }
-                break;
+                    break;
                 default:
-                    let err = new Error('You can only pass create, read, update or delete operations as an arg');
+                    const err = new Error('You can only pass create, read, update or delete operations');
                     err.statusCode = 400;
                     throw err;
             }
@@ -57,37 +57,43 @@ class Model {
         }
     }
 
-    async create (document) {
-        let dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
-        return this._tryCatchFinally(dbConnect.collection.insertOne(document),'create', dbConnect.client)
+    async create(document) {
+        const dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
+        const opPromise = dbConnect.collection.insertOne(document);
+        return this._tryCatchFinally(opPromise, 'create', dbConnect.client);
     }
 
-    async read (id) {
-        let dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
-        let idObject = id ? {"_id": new mongo.ObjectId(id)} : null;
-        return idObject ? this._tryCatchFinally(dbConnect.collection.findOne(idObject),'read', dbConnect.client)
-            : this._tryCatchFinally(dbConnect.collection.find().toArray(),'read', dbConnect.client) ;
+    async read(id) {
+        const dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
+        const idObject = id ? {"_id": new mongo.ObjectId(id)} : null;
+        const opPromise = idObject ? dbConnect.collection.findOne(idObject)
+            : dbConnect.collection.find().toArray();
+        return this._tryCatchFinally(opPromise, 'read', dbConnect.client);
 
     }
 
-    async readByIDsArray (idsArray) {
-        let dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
-        return this._tryCatchFinally(dbConnect.collection.find({_id: { $in: idsArray.map(el => new mongo.ObjectId(el))}}).toArray(),'read', dbConnect.client) ;
+    async readByIDsArray(idsArray) {
+        const dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
+        const arrayOfIdsObjects = idsArray.map(el => new mongo.ObjectId(el));
+        const opPromise = dbConnect.collection.find({_id: {$in: arrayOfIdsObjects}}).toArray();
+        return this._tryCatchFinally(opPromise, 'read', dbConnect.client);
     }
 
-    async update (id, data) {
-        let dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
-        return this._tryCatchFinally(dbConnect.collection.updateMany({"_id": new mongo.ObjectId(id)}, {$set:data}), 'update', dbConnect.client)
+    async update(id, data) {
+        const dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
+        const _id = new mongo.ObjectId(id);
+        const opPromise = dbConnect.collection.updateMany({"_id": _id}, {$set: data});
+        return this._tryCatchFinally(opPromise, 'update', dbConnect.client);
     }
 
-    async delete (id) {
-        let dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
-        return this._tryCatchFinally(await dbConnect.collection.deleteOne({"_id": new mongo.ObjectID(id)}), 'delete', dbConnect.client)
+    async delete(id) {
+        const dbConnect = await this._connectToDB(MongoClient, db, this.collectionName);
+        const _id = new mongo.ObjectID(id);
+        const opPromise = dbConnect.collection.deleteOne({"_id": _id});
+        return this._tryCatchFinally(opPromise, 'delete', dbConnect.client);
     }
 }
 
-function createModel (options) {
+module.exports.createModel = (options) => {
     return new Model(options);
-}
-
-module.exports.createModel = createModel;
+};
